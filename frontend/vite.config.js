3719ce47e3a4f05cc6b6b8c558020c7835c778b0
Ankur Aditya <97215241+ankur7aditya@users.noreply.github.com>
2025-04-10 15:10:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.VITE_BACKEND_URL': JSON.stringify(env.VITE_BACKEND_URL),
-    },
-    server: {
-      port: 3000,
-      proxy: {
-        '/api': {
-          changeOrigin: true,
-          target: env.VITE_BACKEND_URL || 'http://localhost:5000',
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        changeOrigin: true,
+        target: env.VITE_BACKEND_URL || 'http://localhost:5000',
       },
     },
-  };
+  },
 }); 
